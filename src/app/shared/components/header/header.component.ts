9813bd2e4e9d5a26d84c33c2e1eb6b7782516490
Nahuel Garrido <garridonahuel8@gmail.com>
2023-10-21 16:18:29 -0300

    sidebar desplegable

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,6 +6,31 @@ import { Router } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isSidebarVisible: boolean = false;
+  windowInnerWidth: number = window.innerWidth;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenWidth();
+  }
+
   constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.checkScreenWidth();
+  }
+
+  public sidebarHandler(): void {
+    this.isSidebarVisible = !this.isSidebarVisible;
+    console.log(this.isSidebarVisible);
+  }
+
+  public closeSidebar(): void {
+    this.isSidebarVisible = false;
+  }
+
+  private checkScreenWidth() {
+    this.isSidebarVisible = false;
+  }
 }

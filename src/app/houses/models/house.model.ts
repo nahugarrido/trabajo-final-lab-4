f917667e00535a50afeeb3ab28d49860184b1c91
Pablo <pablo.baldor91@gmail.com>
2023-10-21 17:24:21 -0300

    adds houses services

---
+++ src/app/houses/models/house.model.ts
@@ -0,0 +1,16 @@
+export interface House {
+    id: number;
+    price: number;
+    address: Address;
+    parking: number;
+    bathrooms: number;
+    bedrooms: number;
+    description: string;
+    image: string;
+}
+
+interface Address {
+    street: string;
+    district: string;
+    country: string;
+}
